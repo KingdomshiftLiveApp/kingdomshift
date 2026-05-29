d116fd9b21f1afc97357d6ba31edaa5abfa15fb0
KingdomshiftLiveApp <godinfluencedceo@gmail.com>
2026-05-29 13:26:23 -1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE='ks-v4';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
