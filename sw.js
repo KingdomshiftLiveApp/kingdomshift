71613d229883234b7ed4802e4088f11aff4baa54
KingdomshiftLiveApp <godinfluencedceo@gmail.com>
2026-05-29 21:06:34 -1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ks-v7';
+const CACHE='ks-v8';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));});
 self.addEventListener('fetch',e=>{
