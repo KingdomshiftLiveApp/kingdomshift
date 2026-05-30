9eb7876ef4846e47978ffd6873543fc67c505090
KingdomshiftLiveApp <godinfluencedceo@gmail.com>
2026-05-29 21:40:51 -1000

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE='ks-v9';
-self.addEventListener('install',e=>{self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));});
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
